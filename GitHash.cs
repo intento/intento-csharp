466eee3c76c56b9b44853dbc33b48d355fe56bc4
Anton Chizhov <anton@chizhov.com>
2019-07-14 09:33:09 +0300

    GitHash.cs ixcluded from Git

--- GitHash.cs
+++ GitHash.cs
@@ -1,5 +1,5 @@
 namespace IntentoSDK { 
    static internal class GitHash    {    
        public const string hash =           
-"9f27f276b06a7daa7e64912dea9eb0a281c2895b"                                   
+"ebc32afd3efcf004517dd4e1fe6cb9d618951796"                                   
            ;}}                               
