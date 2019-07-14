5367a48772ff268c51420b5d1e7ea2eccf4f6ba6
Anton Chizhov <anton@chizhov.com>
2019-07-14 22:27:45 +0300

    Version info

--- Intento.cs
+++ Intento.cs
@@ -10,6 +10,33 @@ using System.Net;
 using System.Reflection;
 using System.Diagnostics;
 
+
+// Revision history 
+// 1.1.0: Public version
+// 1.1.1: 2019-01-06
+//   - Corrected behaviour for async-wait-async request with sandbox apikey or error in validation of prarmeters. 
+//     In this case no operation id returned from IntentoAPI, but result (in case of sandbox key) or error information. 
+// 1.1.2: 2019-02-02
+//   - More correct processing of text parameter of Fulfill operation
+// 1.1.3: 2019-03-31
+//   - Added methods for obtaining models and glossaries from the provider
+// 1.2.1: 2019-05-28
+//   - Smart delays in wait-async mode
+// 1.2.2: 2019-06-08
+//   - Logging callback.
+//   - Call logging callback for successfull and non-susscessfull Intento API calls. 
+//   - try-catch around calls to http to write log. 
+// 1.2.3: 2019-06-25
+// - Bug with extracting version from dll        
+// 1.2.4: 2019-07-02
+// - waitAsyncDelay
+// 1.2.5: 2019-07-03
+// - The version in useragent now has a commit hash in git
+// - Auxiliary public methods for extracting information from the assembly
+// 1.2.6: 2019-07-14
+// - Minor changes in processing of Modes and Glossaries operations. 
+
+
 namespace IntentoSDK
 {
     /// <summary>
@@ -24,29 +51,6 @@ namespace IntentoSDK
         internal string version;
         internal Action<string, string, Exception> loggingCallback;
 
-        // Revision history 
-        // 1.1.0: Public version
-        // 1.1.1: 2019-01-06
-        //   - Corrected behaviour for async-wait-async request with sandbox apikey or error in validation of prarmeters. 
-        //     In this case no operation id returned from IntentoAPI, but result (in case of sandbox key) or error information. 
-        // 1.1.2: 2019-02-02
-        //   - More correct processing of text parameter of Fulfill operation
-        // 1.1.3: 2019-03-31
-        //   - Added methods for obtaining models and glossaries from the provider
-        // 1.2.1: 2019-05-28
-        //   - Smart delays in wait-async mode
-        // 1.2.2: 2019-06-08
-        //   - Logging callback.
-        //   - Call logging callback for successfull and non-susscessfull Intento API calls. 
-        //   - try-catch around calls to http to write log. 
-        // 1.2.3: 2019-06-25
-        // - Bug with extracting version from dll        
-        // 1.2.4: 2019-07-02
-        // - waitAsyncDelay
-        // 1.2.5: 2019-07-03
-        // - The version in useragent now has a commit hash in git
-        // - Auxiliary public methods for extracting information from the assembly
-        
         /// <summary>
         /// 
         /// </summary>
