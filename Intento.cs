ddb8fac15505fb5366ce75b77a62a28801e4ec03
Anton Chizhov <anton@chizhov.com>
2019-10-08 17:09:52 +0300

    More memoQ changes

--- Intento.cs
+++ Intento.cs
@@ -126,10 +126,12 @@ namespace IntentoSDK
             {
                 Log(string.Format("CheckAsyncJobAsync-2: {0}ms", asyncId));
 
+
                 // async operations inside
                 Log(string.Format("CheckAsyncJobAsync-3: {0}ms", asyncId));
                 result = await client.GetAsync(string.Format("operations/{0}", asyncId));
             }
+
             Log(string.Format("CheckAsyncJobAsync-4: {0}ms", asyncId));
             return result;
         }
