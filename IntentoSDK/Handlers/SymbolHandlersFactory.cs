84298bbf6c8689f555e2effb23b6808db45abc77
Roman Akhmedianov <39262880+oerodger@users.noreply.github.com>
2021-04-29 23:07:02 +0400

    Release/1.5.4 (#10)
    CONTMS-116 add syncwrapper (#8)
    CONTMS-120 Trados: fix bug with < > in Deepl
    
    Co-authored-by: Vladimir Abramov <mr.vovan@GMAIL.COM>

---
+++ IntentoSDK/Handlers/SymbolHandlersFactory.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntentoSDK.Handlers
+{
+    /// <summary>
+    /// Factory for handlers
+    /// </summary>
+    public class SymbolHandlersFactory
+    {
+        private readonly ISymbolHandler[] symbolHandlers;
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public SymbolHandlersFactory()
+        {
+            symbolHandlers = new ISymbolHandler[] {
+                new XmlSymbolHandler(),
+                new HtmlSymbolHandler()
+            };
+        }
+
+        /// <summary>
+        /// Prepare source
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        public string HandleSource(string text, string format)
+        {
+            foreach (ISymbolHandler handler in symbolHandlers.Where(h => h.Format == format))
+            {
+                text = handler.OnSending(text);
+            }
+            return text;
+        }
+
+        /// <summary>
+        /// Prepare result
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        public string HandleResult(string text, string format)
+        {
+            foreach (ISymbolHandler handler in symbolHandlers.Where(h => h.Format == format))
+            {
+                text = handler.OnResponsing(text);
+            }
+            return text;
+        }
+
+        private static readonly SymbolHandlersFactory current = new SymbolHandlersFactory();
+
+        /// <summary>
+        /// Current instance of factory
+        /// </summary>
+        public static SymbolHandlersFactory Current => current;
+        
+    }
+}
