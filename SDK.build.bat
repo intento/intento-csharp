2811d23fbd70edd37be48123906403f1210a91c2
Roman Akhmedyanov <roman.akhmedyanov@inten.to>
2022-05-04 18:33:45 +0400

    Downgrade Autofac

--- SDK.build.bat
+++ SDK.build.bat
@@ -2,7 +2,7 @@
 
 set Configuration=Release
 set DoSign=1
-set Version=2.1.3-beta
+set Version=2.1.4-beta
 
 dotnet build SDK.build.proj /p:Configuration=%Configuration% /p:DoSign=%DoSign% /p:Version=%Version% /fileLogger
 
