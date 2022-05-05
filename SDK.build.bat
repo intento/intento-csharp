1dbf31f48c2db1a8dda900504ab6fd4c2c32a03e
Roman Akhmedyanov <roman.akhmedyanov@inten.to>
2022-05-05 15:49:51 +0400

    Set release version Intento.SDK

--- SDK.build.bat
+++ SDK.build.bat
@@ -2,7 +2,7 @@
 
 set Configuration=Release
 set DoSign=1
-set Version=2.1.5-beta
+set Version=2.2.0
 
 dotnet build SDK.build.proj /p:Configuration=%Configuration% /p:DoSign=%DoSign% /p:Version=%Version% /fileLogger
 
