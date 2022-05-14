7ee9e5be6ddc731beb410419b1fb814c79a3250f
Roman Akhmedyanov <roman.akhmedyanov@inten.to>
2022-05-14 18:17:52 +0400

    CON-2171 Add lite DI

--- SDK.build.bat
+++ SDK.build.bat
@@ -2,7 +2,7 @@
 
 set Configuration=Release
 set DoSign=1
-set Version=2.2.3
+set Version=2.2.4
 
 dotnet build SDK.build.proj /p:Configuration=%Configuration% /p:DoSign=%DoSign% /p:Version=%Version% /fileLogger
 
