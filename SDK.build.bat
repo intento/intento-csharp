2d71e76caf2f6862ee977654819033a513ff8efb
Roman Akhmedyanov <roman.akhmedyanov@inten.to>
2022-07-20 16:44:33 +0400

    Update version of package

--- SDK.build.bat
+++ SDK.build.bat
@@ -2,7 +2,7 @@
 
 set Configuration=Release
 set DoSign=1
-set Version=2.2.7
+set Version=2.2.10
 
 dotnet build SDK.build.proj /p:Configuration=%Configuration% /p:DoSign=%DoSign% /p:Version=%Version% /fileLogger
 
